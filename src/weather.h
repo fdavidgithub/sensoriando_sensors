d0cf4997483eef88a57fa4d237a85227727c0eb8
francis DAVID <contato@fdavid.com.br>
2021-04-02 23:25:08 -0300

    Move UUID to ino

--- src/weather.h
+++ src/weather.h
@@ -19,7 +19,6 @@
 #define KELVIN(x)       ((x) + 273.15)
 
 //Uniques for each hardware compiled
-#define WEATHER_UUID    "e8989226-9475-4c09-8e53-77f759223a6d"  //uuid of sensor
 #define WEATHER_LEN     2                                       //Account of sensors  
 #define TEMPERATURE_ID  5                                       //id of temperature
 #define HUMIDITY_ID     6                                       //id of himidity
